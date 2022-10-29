fcf4825f38c7241872067d4fff4fdf84b64be512
NgoPhamThanhHieu <npth2002@gmail.com>
2022-10-29 15:44:38 +0700

    Xu ly nut Cong

--- Cal/Form1.cs
+++ Cal/Form1.cs
@@ -6,5 +6,13 @@ namespace Cal
         {
             InitializeComponent();
         }
+
+        private void btCong_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txtSoA.Text);
+            double b = double.Parse(txtSoB.Text);
+            double c = a + b;
+            txtKetQua.Text = c.ToString();
+        }
     }
 }
